4af73e9eb04093b464eb937a2df551c6e45a0e0e
Kunal <meetkasakunal@gmail.com>
2022-09-14 09:32:35 -0700

    templates and static

--- views.py
+++ views.py
@@ -1,5 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def home():
-    return "Hello world"
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
